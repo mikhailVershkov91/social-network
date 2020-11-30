c6a996d66feb058056c4eae8dd73987670d1c15c
mikhailVershkov91 <mikhail.vershkov1991@gmail.com>
2020-11-30 16:00:17 +0200

    follow/unfollow users

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import s from "./Users.module.css";
 import userPhoto from "../../assets/images/avatar.png";
 import { NavLink } from "react-router-dom";
+import * as axios from "axios";
 
 const Users = (props) => {
 	let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
@@ -42,7 +43,21 @@ const Users = (props) => {
 							{u.followed ? (
 								<button
 									onClick={() => {
-										props.unfollow(u.id);
+										axios
+											.delete(
+												`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`,
+												{
+													withCredentials: true,
+													headers: {
+														"API-KEY": "9d0fbf15-9405-4c11-b976-8a14a652cc8f",
+													},
+												}
+											)
+											.then((response) => {
+												if (response.data.resultCode === 0) {
+													props.unfollow(u.id);
+												}
+											});
 									}}
 								>
 									Unfollow
@@ -50,7 +65,22 @@ const Users = (props) => {
 							) : (
 								<button
 									onClick={() => {
-										props.follow(u.id);
+										axios
+											.post(
+												`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`,
+												{},
+												{
+													withCredentials: true,
+													headers: {
+														"API-KEY": "9d0fbf15-9405-4c11-b976-8a14a652cc8f",
+													},
+												}
+											)
+											.then((response) => {
+												if (response.data.resultCode === 0) {
+													props.follow(u.id);
+												}
+											});
 									}}
 								>
 									Follow
