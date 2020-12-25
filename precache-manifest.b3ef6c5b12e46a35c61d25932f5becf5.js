2d1373b5e40d993f21da21b990b1c715a9455852
mikhailVershkov91 <mikhail.vershkov1991@gmail.com>
2020-12-25 11:20:07 +0200

    Updates

---
+++ precache-manifest.b3ef6c5b12e46a35c61d25932f5becf5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05ec7b2eb0a53c9b141c998287cf20af",
+    "url": "/social-network/index.html"
+  },
+  {
+    "revision": "d329ff7d9d4900b2d676",
+    "url": "/social-network/static/css/main.2dc07e0a.chunk.css"
+  },
+  {
+    "revision": "5ec093738b43aaa0bce5",
+    "url": "/social-network/static/js/2.4dc93e16.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/social-network/static/js/2.4dc93e16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d329ff7d9d4900b2d676",
+    "url": "/social-network/static/js/main.ca53b6e9.chunk.js"
+  },
+  {
+    "revision": "68b01136d249e5809ba9",
+    "url": "/social-network/static/js/runtime-main.bf681233.js"
+  },
+  {
+    "revision": "54e9ec5365eeb967838ffd2a35eda50b",
+    "url": "/social-network/static/media/avatar.54e9ec53.png"
+  }
+]);
